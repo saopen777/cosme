946bf8cfe521bfd410efeac80a364e8c0e17f364
saopen777 <saopen@icloud.com>
2020-10-05 16:06:46 +0900

    show調整

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "tweet#index"
   resources :tweet do
-    resources :comment, only: :create
+    resources :comments, only: :create
   end
+  resources :users, only: :show
 end
--- db/migrate/20200911015232_create_comments.rb
+++ db/migrate/20200911015232_create_comments.rb
@@ -2,9 +2,10 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text :text,null: false
-      t.references :user, null: false, foreign_key: true
-      t.references :tweet, null: false, foreign_key: true
+      t.references :user_id, null: false, foreign_key: true
+      t.references :tweet_id, null: false, foreign_key: true
       t.timestamps
+      
     end
   end
 end
